13dec5a1a65b2644d6ea48b507992ed43bd394d5
artisticsciencex <artisticsciencex@gmail.com>
2025-09-30 04:28:28 +0000

    Deploying to gh-pages from @ artisticsciencex/artisticsciencex.github.io@d556817bc0034d37eed08e7572ea1e74400c31f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,8 +459,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/en-us/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-one-paper-beyond-symbols-motion-perception-cues-enhance-dual-task-performance-with-wearable-directional-guidance-has-been-accepted-by-ubicomp-iswc-2025",
+          title: 'One paper, “Beyond Symbols: Motion Perception Cues Enhance Dual-Task Performance with Wearable Directional...',
           description: "",
           section: "News",},{id: "projects-project-7",
           title: 'project 7',
