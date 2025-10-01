928c4143e1effc7b634d66c5760e65a362f2daaa
artisticsciencex <artisticsciencex@gmail.com>
2025-10-01 10:36:35 +0000

    Deploying to gh-pages from @ artisticsciencex/artisticsciencex.github.io@e3dbacd50f183d01dca5024faa0565abcbc7d0f9 🚀

--- fr-ca/assets/js/search-data.js
+++ fr-ca/assets/js/search-data.js
@@ -55,21 +55,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/fr-ca/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "déroulant",
-              handler: () => {
-                window.location.href = "/fr-ca/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "déroulant",
-              handler: () => {
-                window.location.href = "/fr-ca/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
