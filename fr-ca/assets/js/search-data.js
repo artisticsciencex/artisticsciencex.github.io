f2ec0fe5d8f96d047e0204b141e095b6a8483f22
artisticsciencex <artisticsciencex@gmail.com>
2025-10-01 09:26:49 +0000

    Deploying to gh-pages from @ artisticsciencex/artisticsciencex.github.io@c49996b2a3cb6d43332838ee695ff48103f7357c 🚀

--- fr-ca/assets/js/search-data.js
+++ fr-ca/assets/js/search-data.js
@@ -62,13 +62,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/fr-ca/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "menu de navigation",
-          handler: () => {
-            window.location.href = "/fr-ca/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
